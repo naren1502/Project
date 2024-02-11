0a31c04977e4291783c4f1196763c28a2ebc453c
naren1502 <narenthirans@outlook.com>
2024-02-11 15:24:48 +0530

    Javautility and FileUtility updated

--- src/main/java/genricUtility/FileUtility.java
+++ src/main/java/genricUtility/FileUtility.java
@@ -1,5 +1,17 @@
 package genricUtility;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class FileUtility {
 
+	public String readDataFromPropertyfile(String key) throws IOException {
+		File file=new File("");
+		FileInputStream fis=new FileInputStream(file);
+		Properties property=new Properties();
+		property.load(fis);
+		return property.getProperty(key);			
+	}
 }
